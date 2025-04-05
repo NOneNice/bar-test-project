583f93da43a67a21f22393a96fd87097425366a3
Alexey Bobrov <q20100@mail.ru>
2025-04-05 16:54:19 +0500

    Первоначальный хедер

--- src/app/modules/header/header.component.ts
+++ src/app/modules/header/header.component.ts
@@ -1,13 +1,25 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { InputText } from 'primeng/inputtext';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Button } from 'primeng/button';
+import { NgOptimizedImage } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    InputText,
+    FormsModule,
+    ReactiveFormsModule,
+    Button,
+    NgOptimizedImage,
+    MatIcon,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
-
+  protected formControl = new FormControl('');
 }
